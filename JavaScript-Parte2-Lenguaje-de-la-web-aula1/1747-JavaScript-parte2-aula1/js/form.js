b3bf474d4410bd426e1b023266977d7c5d3d63dd
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-05-02 21:01:28 -0300

    Creando esqueleto proyecto codificador desafío 1

--- JavaScript-Parte2-Lenguaje-de-la-web-aula1/1747-JavaScript-parte2-aula1/js/form.js
+++ JavaScript-Parte2-Lenguaje-de-la-web-aula1/1747-JavaScript-parte2-aula1/js/form.js
@@ -5,21 +5,25 @@ botonAdicionar.addEventListener("click",function(event){
 
     var form = document.querySelector("#form-adicionar"); 
     var paciente = capturarDatosPaciente(form);
-    var pacienteTr = construirTr(paciente);
     // validar paciente
     var errores = validarPaciente(paciente);
     if(errores.length > 0){
         exhibirMensajesErrores(errores);
         return;
     }
-    var tabla = document.querySelector("#tabla-pacientes");
-    tabla.appendChild(pacienteTr);
+    adicionarPacienteEnLaTabla(paciente);
     form.reset();
 
     var mensajeErrores = document.querySelector("#mensajes-errores");
     mensajeErrores.innerHTML = "";
 
 });
+
+function adicionarPacienteEnLaTabla(paciente){
+    var pacienteTr = construirTr(paciente);
+    var tabla = document.querySelector("#tabla-pacientes");
+    tabla.appendChild(pacienteTr);
+}
 
 function capturarDatosPaciente(form){
     //capturando los datos del formulario
